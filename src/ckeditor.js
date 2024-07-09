bcc7d6ec47eda8390c061d1ac27e9fb63f5238a5
mouellet <mouellet@cyberimpact.com>
2024-07-09 10:07:46 -0400

    Most of required plugins

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -6,6 +6,12 @@
 // The editor creator to use.
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
+import Clipboard from '@ckeditor/ckeditor5-clipboard/src/clipboard';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
@@ -14,15 +20,23 @@ import List from '@ckeditor/ckeditor5-list/src/list';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters';
+import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
+	Underline,
+	Strikethrough,
+	CKFinder, // Image
+	Superscript,
+	Clipboard,
+	RemoveFormat,
+	SpecialCharacters,
+	SourceEditing,
 	Essentials,
 	Heading,
-	Underline,
 	Bold,
 	Italic,
 	Link,
