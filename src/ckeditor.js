3d8694a400f2738a2b5e7b8914f5bd8844a037bc
mouellet <mouellet@cyberimpact.com>
2024-07-09 10:27:54 -0400

    Comment out plugins

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -6,12 +6,16 @@
 // The editor creator to use.
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
-import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
-import Clipboard from '@ckeditor/ckeditor5-clipboard/src/clipboard';
-import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
+// import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+// import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
+// import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
+// import Clipboard from '@ckeditor/ckeditor5-clipboard/src/clipboard';
+// import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
+// import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters';
+// import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting';
+
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
@@ -19,24 +23,22 @@ import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters';
-import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
-	Underline,
-	Strikethrough,
-	CKFinder, // Image
-	Superscript,
-	Clipboard,
-	RemoveFormat,
-	SpecialCharacters,
-	SourceEditing,
-	Essentials,
 	Heading,
+	Underline,
+	// Strikethrough,
+	// CKFinder, // Image
+	// Superscript,
+	// Clipboard,
+	// RemoveFormat,
+	// SpecialCharacters,
+	// SourceEditing,
+
+	Essentials,
 	Bold,
 	Italic,
 	Link,
